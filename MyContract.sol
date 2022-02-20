b1094979514c49579ef360e1968eee3608a4fdc0
dolambinhminh <ocsen.hoc.code@gmail.com>
2022-02-20 20:28:20 +0700

    MyContract.sol

---
+++ MyContract.sol
@@ -0,0 +1,30 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract MyContract {
+    address private minter;
+    mapping(address => uint) public balances;
+    modifier checkMinter {
+        require(minter == msg.sender, 'You have permission setup balace');
+        _;
+    }
+    modifier checkBalance(uint amount) {
+        require(balances[msg.sender] >= amount, "You don't have enough balance");
+        _;
+    }
+    constructor() {
+        minter = msg.sender;
+    }
+
+    function set(uint amount) public checkMinter{
+        balances[msg.sender] += amount;
+    }
+
+    function get(address add) public view returns (uint) {
+        return balances[add];
+    }
+
+    function send(address receiver, uint amount) public checkBalance(amount){
+        balances[msg.sender]-= amount;
+        balances[receiver]+= amount;
+    }
+}
